e4af1e4e68c438b0d2889e4ac7546288c7305fd4
Jamie vullo <jvullo@protonmail.com>
2020-07-20 20:37:36 -0400

    Added more functionality gf says look fantasstic-ish

--- app.js
+++ app.js
@@ -6,12 +6,29 @@ document.addEventListener("DOMContentLoaded", function() {
 let buttons = document.querySelectorAll(".button")
 let total = 0 
 let storedNumber = 0
+let operator = ""
 let currentNumber = ""
+let isOperator = false
 
 buttons.forEach(button => {
     return button.addEventListener("click", (e) => {
         let buttonPressed = e.target.innerText
-        displayNumber(buttonPressed)
+
+        if(buttonPressed === "+" || buttonPressed === "-" || buttonPressed === "/" || buttonPressed === "x") {
+            storeTheDisplayedNumberAndOperator(buttonPressed)
+        } else if (buttonPressed === "=") {
+            equals()
+        } else if (buttonPressed === "CE") {
+            clear()
+        } else {
+            if(isOperator === false) {
+                displayNumber(buttonPressed)
+            } else {
+                currentNumber = ""
+                isOperator = false
+                displayNumber(buttonPressed)
+            }
+        }        
     })
 })
 
@@ -24,7 +41,20 @@ function displayNumber(buttonPressed) {
 
 }
 
-function storeTheDisplayedNumber() {
+function storeTheDisplayedNumberAndOperator(buttonPressed) {
+    if(buttonPressed === "+" || buttonPressed === "-" || buttonPressed === "/" || buttonPressed === "x") {
+        operator = buttonPressed
+        storedNumber = parseInt(currentNumber)
+        isOperator = true
+        console.log(storedNumber, operator)
+    }
+}
+
+function clear() {
+
+}
+
+function equals() {
 
 }
 

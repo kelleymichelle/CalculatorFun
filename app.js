f47898b8ef93ee1bae5e8df1fa13c9ebae722302
Kelley Chaplain <kmchaplain@gmail.com>
2020-07-21 18:37:39 -0500

    added second toggle to show if this is the first time an operator is pressed and there is no total yet or not

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
 let buttons = document.querySelectorAll(".button")
 let numberWindow = document.getElementById("display-content")
 let total = 0 
+//when currentNumber gets an operator
 let storedNumber = 0
 let operator = ""
 let currentNumber = ""
 let isOperator = false
+let firstShow = true
 
 buttons.forEach(button => {
     return button.addEventListener("click", (e) => {
@@ -49,6 +51,7 @@ function storeTheDisplayedNumberAndOperator(buttonPressed) {
         // total = parseInt(currentNumber)
         isOperator = true
         console.log(storedNumber, operator, total)
+        equals()
     }
 }
 
@@ -59,36 +62,56 @@ function clear() {
     currentNumber = ""
     isOperator = false
     numberWindow.innerText = "0"
+    firstShow = true
 }
 
 function equals() {
     // storedNumber = parseInt(currentNumber)
-    console.log(storedNumber, total, currentNumber)
-    if(operator === "+") {
-        let sum = total + parseInt(currentNumber)
-        numberWindow.innerText = sum
-        storedNumber = sum
-        total = sum
-    } else if (operator === "-") {
-        let difference = total - parseInt(currentNumber)
-        numberWindow.innerText = difference
-        storedNumber = difference
-        total = difference
-    } else if (operator === "/") {
-        let result = total / parseInt(currentNumber)
-        console.log(result)
-        numberWindow.innerText = result
-        storedNumber = result
-        total = result
-    } else if (operator === "x") {
-        let product = total * parseInt(currentNumber)
-        numberWindow.innerText = product
-        storedNumber = product
-        total = product
-    } 
+    let currentInt = parseInt(currentNumber)
+    
+    if (firstShow) {
+        numberWindow.innerText = currentInt
+        storedNumber = currentInt
+        total = currentInt
+        firstShow = false
+    } else {
+
+        console.log(storedNumber, total, currentNumber)
+        if(operator === "+") {
+            let sum = total + parseInt(currentNumber)
+            numberWindow.innerText = sum
+            storedNumber = sum
+            total = sum
+        } else if (operator === "-") {
+            
+            let difference = total - parseInt(currentNumber)
+            // console.log(difference)
+            numberWindow.innerText = difference
+            storedNumber = difference
+            total = difference
+        } else if (operator === "/") {
+            if (total === 0) {
+                numberWindow.innerText = currentInt
+                storedNumber = currentInt
+                total = currentInt
+            } else {
+
+                let result = total / parseInt(currentNumber)
+                console.log(result)
+                numberWindow.innerText = result
+                storedNumber = result
+                total = result
+            }
+        } else if (operator === "x") {
+            let product = total * parseInt(currentNumber)
+            numberWindow.innerText = product
+            storedNumber = product
+            total = product
+        } 
     currentNumber = total
     // isOperator = false
     console.log(storedNumber, total, currentNumber)
+    }
 }
 
 

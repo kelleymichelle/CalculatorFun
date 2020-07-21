94776c6719bbd9f38137f53462765192dcb81fe9
Jamie vullo <jvullo@protonmail.com>
2020-07-20 20:13:02 -0400

    Added some functionality to getting clicked numbers and displaying them.

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+        
 })
+
+
+let buttons = document.querySelectorAll(".button")
+let total = 0 
+let storedNumber = 0
+let currentNumber = ""
+
+buttons.forEach(button => {
+    return button.addEventListener("click", (e) => {
+        let buttonPressed = e.target.innerText
+        displayNumber(buttonPressed)
+    })
+})
+
+function displayNumber(buttonPressed) {
+    currentNumber += buttonPressed
+    console.log(currentNumber)
+
+    let numberWindow = document.getElementById("display-content")
+    numberWindow.innerText = currentNumber
+
+}
+
+function storeTheDisplayedNumber() {
+
+}
+
+// function operatorPressed(operator, storedNumber) {
+//     if(operator === "+") {
+//         //sumthin with the total
+//     } else if (operator === "-") {
+//         //sumthin with the total
+//     } else if (operator === "/") {
+//         // sonmething with the total
+//     } else if (operator === "x") {
+//         // somethin with the total
+//     } else if (operator === "=") {
+//         // sometin with the total
+//     } else if (operator === "CE") {
+//         // clear out the total
+//     }
+// }
+
+
+
+
